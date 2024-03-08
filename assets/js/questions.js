9f36ea075f3ce2b766e0fa773efe994568526a60
DominicKepes <dominickepes1@gmail.com>
2024-03-07 20:19:24 -0800

    add finished project to repo

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'Inside which HTML element do we put the JavaScript?',
+    choices: ['<javascript>', '<js>', '<script>', '<sc>'],
+    answer: '<script>',
+  },
+  {
+    title: 'Where is the correct place to insert a JavaScript?',
+    choices: ['The <body> section', 'The <head> section'],
+    answer: 'The <body> section',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title: 'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+  {
+    title: 'Java and Javascript are the same',
+    choices: ['true', 'false'],
+    answer: 'false'
+  }
+];
